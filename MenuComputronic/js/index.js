671e20f78761fbf8d3ea4ae630af3000b6632fc2
InstitutoComputronic <38534040+InstitutoComputronic@users.noreply.github.com>
2018-04-20 17:19:17 -0500

    Add files via upload

---
+++ MenuComputronic/js/index.js
@@ -0,0 +1,6 @@
+var btn = document.getElementById('boton');
+var menu = document.getElementById('Menu-Computronic');
+
+btn.addEventListener('click', function () {
+    menu.classList.toggle('mover');
+});
